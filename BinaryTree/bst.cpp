8333a6832fa0f65889753b8df017a9dafd281379
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-10-27 11:27:27 +0530

    leftRotate, rightRotate functions added

--- BinaryTree/bst.cpp
+++ BinaryTree/bst.cpp
@@ -20,6 +20,63 @@ int height(Node* root){
     return root->height;
 }
 
+Node* leftRotate(Node* node){
+    Node* newRoot = node->right;
+    Node* temp = newRoot->left;
+
+    newRoot->left = node;
+    node->right = temp;
+
+    node->height = 1 + max(height(node->left), height(node->right));
+    newRoot->height = 1 + max(height(newRoot->left), height(newRoot->right));
+
+    return newRoot;
+    
+}
+Node* rightRotate(Node* node){
+    Node* newRoot = node->left;
+    Node* temp = newRoot->right;
+
+    newRoot->right = node;
+    node->left = temp;
+
+    node->height = 1 + max(height(node->left), height(node->right));
+    newRoot->height = 1 + max(height(newRoot->left), height(newRoot->right));
+
+    return newRoot;
+}
+
+Node* rotate(Node* root){
+    int balance = height(root->left)-height( root->right);
+
+    if(balance>1){
+        //Left Heavy (2 cases)
+        if(height(root->left->left) - height(root->left->right) >=0){
+            //Left-Left Case
+            return rightRotate(root);
+        }
+        else{
+            //Left-Right Case
+            root->left = leftRotate(root->left);
+            return rightRotate(root);
+
+        }
+    }
+    else if(balance<-1){
+        //Right Heavy
+        if(height(root->right->right) - height(root->right->left) >=0){
+            //Right-Right Case
+            return leftRotate(root);
+        }
+        else{
+            //Right-Left Case
+            root->right = rightRotate(root->right);
+            return leftRotate(root);
+        }
+    }
+    return root;
+}
+
 Node* insert(Node* root, int val){
     if(root == nullptr){
         return new Node(val);
@@ -31,7 +88,8 @@ Node* insert(Node* root, int val){
         root->right = insert(root->right, val);
     }
     root->height = 1 + max(height(root->left), height(root->right));
-    return root;
+    return rotate(root);
+    // return root;
 }
 
 bool isbalanced(Node* root){
@@ -62,7 +120,10 @@ int main(){
     root = insert(root, 70);
     root = insert(root, 80);
     root = insert(root, 90);
-    root = insert(root, 100);
+    root = insert(root, 130);
+    root = insert(root, 140);
+    root = insert(root, 150);
+    root = insert(root, 160);
 
     cout<<"BST Inorder traversal: ";
     inorder(root);
